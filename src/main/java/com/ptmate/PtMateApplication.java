89be8ea0f0eb204e8d1aedd0f4a374da797dc5cb
ymkim97 <rladudaud97@naver.com>
2024-12-20 20:43:00 +0900

    chore: Checkstyle, editorconfig 세팅

---
+++ src/main/java/com/ptmate/PtMateApplication.java
@@ -0,0 +1,21 @@
+package com.ptmate;
+
+import java.util.TimeZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class PtMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtMateApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+}
