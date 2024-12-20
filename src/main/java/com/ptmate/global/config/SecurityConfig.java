b6ad9da0f5a239901ea29bed091e59cd16309aff
ymkim97 <rladudaud97@naver.com>
2024-12-20 20:43:00 +0900

    chore: Checkstyle, editorconfig 세팅

---
+++ src/main/java/com/ptmate/global/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.ptmate.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private static final String[] ALLOWED_URIS = {
+		"/",
+		"/login",
+		"/index.html",
+		"/api/oauth2/**"
+	};
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.cors(Customizer.withDefaults())
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.csrf(AbstractHttpConfigurer::disable)
+			.headers(headers ->
+				headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+			.sessionManagement(sessionManagement ->
+				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+			.authorizeHttpRequests(request ->
+				request.requestMatchers(ALLOWED_URIS).permitAll()
+					.anyRequest().authenticated());
+
+		return http.build();
+	}
+}
