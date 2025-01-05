38ee34f16fa2b1464fa924b015d808a88496a541
fakerdeft <whakswp123@naver.com>
2025-01-06 00:17:05 +0900

    [TNT-28] refactor: repository 어노테이션 추가

--- src/main/java/com/tnt/domain/member/repository/MemberRepository.java
+++ src/main/java/com/tnt/domain/member/repository/MemberRepository.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tnt.domain.member.Member;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByIdAndDeletedAt(Long id, LocalDateTime deletedAt);
