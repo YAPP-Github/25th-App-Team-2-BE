1ac7bde7b88f8c0902325f34b6482708a2886d4e
fakerdeft <whakswp123@naver.com>
2025-01-06 01:22:29 +0900

    [TNT-28] refactor: 401 예외 추가, 접근 제어자 수정

--- src/main/java/com/tnt/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/tnt/global/error/handler/GlobalExceptionHandler.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
+import com.tnt.global.error.exception.TnTException;
+import com.tnt.global.error.exception.UnauthorizedException;
 import com.tnt.global.error.model.ErrorResponse;
 
 import jakarta.validation.ConstraintViolationException;
@@ -33,7 +35,7 @@ public class GlobalExceptionHandler {
 	// 필수 파라미터 예외
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MissingServletRequestParameterException.class)
-	public ErrorResponse handleMissingServletRequestParameter(
+	protected ErrorResponse handleMissingServletRequestParameter(
 		MissingServletRequestParameterException exception) {
 		log.warn("Required request parameter is missing: {}", exception.getParameterName());
 		String errorMessage = String.format("필수 파라미터 '%s'가 누락되었습니다.", exception.getParameterName());
@@ -44,25 +46,30 @@ public class GlobalExceptionHandler {
 	// 파라미터 타입 예외
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
-	public ErrorResponse handleMethodArgumentTypeMismatch(
+	protected ErrorResponse handleMethodArgumentTypeMismatch(
 		MethodArgumentTypeMismatchException exception) {
+		exception.getRequiredType();
 		log.warn("Type mismatch for parameter: {}. Required type: {}", exception.getName(),
-			exception.getRequiredType() != null ? exception.getRequiredType().getSimpleName() : "unknown");
+			exception.getRequiredType().getSimpleName());
 		String errorMessage;
-		if (exception.getRequiredType() != null) {
-			errorMessage = String.format("파라미터 '%s'의 형식이 올바르지 않습니다. 예상 타입: %s",
-				exception.getName(), exception.getRequiredType().getSimpleName());
-		} else {
-			errorMessage = String.format("파라미터 '%s'의 형식이 올바르지 않습니다.", exception.getName());
-		}
+		exception.getRequiredType();
+		errorMessage = String.format("파라미터 '%s'의 형식이 올바르지 않습니다. 예상 타입: %s",
+			exception.getName(), exception.getRequiredType().getSimpleName());
 
 		return new ErrorResponse(errorMessage);
+	}
+
+	// 401 Unauthorized 예외
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	@ExceptionHandler(UnauthorizedException.class)
+	protected ErrorResponse handleUnauthorizedException(TnTException exception) {
+		return new ErrorResponse(exception.getMessage());
 	}
 
 	// @Validated 있는 클래스에서 @RequestParam, @PathVariable 등에 적용된 제약 조건 예외
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(ConstraintViolationException.class)
-	public ErrorResponse handleConstraintViolationException(ConstraintViolationException exception) {
+	protected ErrorResponse handleConstraintViolationException(ConstraintViolationException exception) {
 		log.warn("Constraint violation: {}", exception.getMessage());
 
 		List<String> errors = exception.getConstraintViolations()
@@ -78,7 +85,7 @@ public class GlobalExceptionHandler {
 	// @Valid, @Validated 있는 곳에서 주로 @RequestBody dto 필드에 적용된 검증 어노테이션 유효성 검사 실패 예외
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ErrorResponse handleMethodArgumentNotValidException(
+	protected ErrorResponse handleMethodArgumentNotValidException(
 		MethodArgumentNotValidException exception) {
 		log.warn(exception.getBindingResult().getAllErrors().getFirst().getDefaultMessage());
 
@@ -91,7 +98,7 @@ public class GlobalExceptionHandler {
 		HttpMessageNotReadableException.class,
 		DateTimeException.class
 	})
-	public ErrorResponse handleDateTimeParseException(DateTimeException exception) {
+	protected ErrorResponse handleDateTimeParseException(DateTimeException exception) {
 		log.warn(exception.getMessage());
 
 		return new ErrorResponse("DateTime 형식이 잘못되었습니다. 서버 관리자에게 문의해 주세요.");
@@ -102,7 +109,7 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(value = {
 		MaxUploadSizeExceededException.class
 	})
-	public ErrorResponse handleCustomBadRequestException(RuntimeException exception) {
+	protected ErrorResponse handleCustomBadRequestException(RuntimeException exception) {
 		log.warn(exception.getMessage());
 
 		return new ErrorResponse(exception.getMessage());
@@ -111,7 +118,7 @@ public class GlobalExceptionHandler {
 	// 기타 500 예외
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(RuntimeException.class)
-	public ErrorResponse handleRuntimeException(RuntimeException exception) {
+	protected ErrorResponse handleRuntimeException(RuntimeException exception) {
 		StringBuilder sb = new StringBuilder();
 
 		for (int i = 0; i < ERROR_KEY_LENGTH; i++) {
