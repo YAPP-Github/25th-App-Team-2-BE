86e860555ba490cafc377cd0e54cf07efcd062f1
fakerdeft <whakswp123@naver.com>
2025-01-07 10:12:41 +0900

    [TNT-28] refactor: 안 쓰는 메서드 제거

--- src/main/java/com/tnt/domain/member/repository/MemberRepository.java
+++ src/main/java/com/tnt/domain/member/repository/MemberRepository.java
@@ -1,8 +1,5 @@
 package com.tnt.domain.member.repository;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +8,4 @@ import com.tnt.domain.member.Member;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-	Optional<Member> findByIdAndDeletedAt(Long id, LocalDateTime deletedAt);
 }
