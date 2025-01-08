4a85d8c56f6b1445990d8ef6a0b6389957dd65b7
fakerdeft <whakswp123@naver.com>
2025-01-08 17:08:30 +0900

    [TNT-83] feat: findBySocialIdAndSocialType 추가

--- src/main/java/com/tnt/infrastructure/mysql/member/repository/MemberRepository.java
+++ src/main/java/com/tnt/infrastructure/mysql/member/repository/MemberRepository.java
@@ -1,11 +1,15 @@
-package com.tnt.domain.member.repository;
+package com.tnt.infrastructure.mysql.member.repository;
+
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tnt.domain.member.Member;
+import com.tnt.domain.member.SocialType;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+	Optional<Member> findBySocialIdAndSocialType(String socialId, SocialType socialType);
 }
