6494d082d7e17e8f7f9fe69ad3be95b5325cd696
fakerdeft <whakswp123@naver.com>
2025-01-09 23:50:42 +0900

    [TNT-83] refactor: OAuthService 필드 @Mock 추가

--- src/test/java/com/tnt/application/member/OAuthServiceTest.java
+++ src/test/java/com/tnt/application/member/OAuthServiceTest.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
@@ -50,7 +49,7 @@ class OAuthServiceTest {
 
 	private MockWebServer mockWebServer;
 
-	@InjectMocks
+	@Mock
 	private OAuthService oAuthService;
 
 	@Mock
