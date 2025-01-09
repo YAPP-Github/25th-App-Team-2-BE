f5713e8b11ec7a524ffac81822b221fccb9df890
fakerdeft <whakswp123@naver.com>
2025-01-10 07:29:12 +0900

    [TNT-83] refactor: 디렉토리 위치 변경

--- src/test/java/com/tnt/domain/member/MemberIntegrationTest.java
+++ src/test/java/com/tnt/domain/member/MemberIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.tnt.infrastructure.mysql.member.repository.MemberRepository;
+import com.tnt.infrastructure.mysql.repository.member.MemberRepository;
 
 @SpringBootTest
 @Transactional
