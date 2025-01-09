1dd283e3da027391acb91d93ed238af92040db11
fakerdeft <whakswp123@naver.com>
2025-01-09 18:15:54 +0900

    [TNT-83] refactor: 에러 메시지 enum 클래스로 이동

--- src/main/java/com/tnt/global/error/model/ErrorMessage.java
+++ src/main/java/com/tnt/global/error/model/ErrorMessage.java
@@ -8,15 +8,22 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorMessage {
 
 	SERVER_ERROR("서버 에러가 발생했습니다."),
+
 	BAD_REQUEST("잘못된 요청입니다."),
 	FAILED_TO_PROCESS_REQUEST("요청 진행에 실패했습니다."),
 	ACCESS_DENIED("접근이 거부되었습니다."),
+	MISSING_REQUIRED_PARAMETER_ERROR("필수 파라미터 '%s'가 누락되었습니다."),
+	PARAMETER_FORMAT_NOT_CORRECT("파라미터 '%s'의 형식이 올바르지 않습니다."),
+	INPUT_VALUE_IS_INVALID("입력값이 유효하지 않습니다."),
+	INVALID_FORMAT_DATETIME("DateTime 형식이 잘못되었습니다."),
 
 	AUTHORIZATION_HEADER_ERROR("Authorization 헤더가 존재하지 않거나 올바르지 않은 형식입니다."),
 	NO_EXIST_SESSION_IN_STORAGE("세션 스토리지에 세션이 존재하지 않습니다."),
 
-	SOCIAL_SERVICE_NOT_SUPPORT("지원하지 않는 소셜 서비스입니다."),
+	UNSUPPORTED_SOCIAL_TYPE("지원하지 않는 소셜 서비스입니다."),
 	FAILED_TO_FETCH_USER_INFO("소셜 서버로부터 유저 정보 불러오기에 실패했습니다."),
+	APPLE_CLIENT_ERROR("Apple 클라이언트 에러가 발생했습니다."),
+	APPLE_SERVER_ERROR("Apple 서버 에러가 발생했습니다."),
 	APPLE_AUTH_ERROR("Apple 인증에 실패했습니다."),
 	FAILED_TO_FETCH_PRIVATE_KEY("시크릿 키 불러오기에 실패했습니다."),
 	FAILED_TO_CREATE_APPLE_CLIENT_SECRET("애플 클라이언트 시크릿 생성 중 에러가 발생했습니다."),
