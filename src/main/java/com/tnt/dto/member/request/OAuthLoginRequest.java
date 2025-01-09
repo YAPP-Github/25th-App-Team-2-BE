180d54f88a0463112f03aef42093c5dad5cb283b
fakerdeft <whakswp123@naver.com>
2025-01-10 07:30:31 +0900

    [TNT-83] refactor: 파라미터 Schema 수정

--- src/main/java/com/tnt/dto/member/request/OAuthLoginRequest.java
+++ src/main/java/com/tnt/dto/member/request/OAuthLoginRequest.java
@@ -5,17 +5,17 @@ import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "소셜 로그인 요청")
 public record OAuthLoginRequest(
-	@Schema(description = "소셜 로그인 타입", example = "KAKAO, APPLE")
+	@Schema(description = "소셜 로그인 타입", example = "KAKAO", type = "string", allowableValues = {"KAKAO", "APPLE"})
 	@NotBlank(message = "소셜 로그인 타입은 필수입니다.")
 	String socialType,
 
-	@Schema(description = "소셜 액세스 토큰", example = "access_token_example")
+	@Schema(description = "소셜 액세스 토큰 (카카오 로그인 시)", example = "atweroiuhoresihsgfkn", type = "string")
 	String socialAccessToken,
 
-	@Schema(description = "인가 코드", example = "authorization_code_example")
+	@Schema(description = "인가 코드 (애플 Android 로그인 시)", example = "1231231231231", type = "string")
 	String authorizationCode,
 
-	@Schema(description = "ID 토큰", example = "id_token_example")
+	@Schema(description = "ID 토큰 (애플 iOS 로그인 시)", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...", type = "string")
 	String idToken
 ) {
 
