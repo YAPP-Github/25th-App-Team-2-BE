98749b6980f32757650ec1b5060d996729907180
fakerdeft <whakswp123@naver.com>
2025-01-10 12:40:50 +0900

    [TNT-83] refactor: extractOAuthUserInfo 메서드 삭제

--- src/main/java/com/tnt/domain/member/SocialType.java
+++ src/main/java/com/tnt/domain/member/SocialType.java
@@ -1,27 +1,15 @@
 package com.tnt.domain.member;
 
-import java.util.Map;
-
-import com.tnt.dto.member.AppleUserInfo;
-import com.tnt.dto.member.KakaoUserInfo;
-import com.tnt.dto.member.OAuthUserInfo;
-
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum SocialType {
-	KAKAO {
-		@Override
-		public OAuthUserInfo extractOAuthUserInfo(Map<String, Object> attributes) {
-			return new KakaoUserInfo(attributes);
-		}
-	},
-	APPLE {
-		@Override
-		public OAuthUserInfo extractOAuthUserInfo(Map<String, Object> attributes) {
-			return new AppleUserInfo(attributes);
-		}
-	};
 
-	public abstract OAuthUserInfo extractOAuthUserInfo(Map<String, Object> attributes);
+	KAKAO("kakao"),
+
+	APPLE("apple");
+
+	private final String registrationId;
 }
