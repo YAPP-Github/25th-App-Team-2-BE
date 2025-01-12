a7c7ab6bc5bc70306f090e02f07e9b121ccd8609
fakerdeft <whakswp123@naver.com>
2025-01-13 01:42:14 +0900

    [TNT-83] refactor: 클래스 @RequestMapping 제거, swagger 내용 수정

--- src/main/java/com/tnt/presentation/member/LoginController.java
+++ src/main/java/com/tnt/presentation/member/LoginController.java
@@ -1,9 +1,9 @@
 package com.tnt.presentation.member;
 
-import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.*;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +16,16 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-@Tag(name = "로그인", description = "로그인 관련 API")
+@Tag(name = "로그인/로그아웃", description = "로그인/로그아웃 관련 API")
 @RestController
-@RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
 
 	private final OAuthService oauthService;
 
-	@Operation(summary = "소셜 로그인")
-	@PostMapping("/oauth")
-	@ResponseStatus(value = HttpStatus.OK)
+	@Operation(summary = "소셜 로그인 API")
+	@PostMapping("/login/oauth")
+	@ResponseStatus(value = OK)
 	public OAuthLoginResponse oauthLogin(@RequestBody @Valid OAuthLoginRequest request) {
 		return oauthService.oauthLogin(request);
 	}
