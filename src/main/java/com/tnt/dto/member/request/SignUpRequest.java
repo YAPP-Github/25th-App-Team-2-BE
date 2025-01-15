d7190d2f6de9c6d886f6354896999ae753e50456
fakerdeft <whakswp123@naver.com>
2025-01-15 23:46:27 +0900

    [TNT-83] feat: 회원가입 요청 dto 구현

---
+++ src/main/java/com/tnt/dto/member/request/SignUpRequest.java
@@ -0,0 +1,49 @@
+package com.tnt.dto.member.request;
+
+import java.time.LocalDate;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+@Schema(description = "회원가입 API 요청")
+public record SignUpRequest(
+	@Schema(description = "회원 타입 (trainer / trainee)", example = "trainer")
+	@NotBlank(message = "회원 타입은 필수입니다.")
+	String memberType,
+
+	@Schema(description = "소셜 로그인 타입 (KAKAO / APPLE)", example = "KAKAO", allowableValues = {"KAKAO", "APPLE"})
+	@NotBlank(message = "소셜 로그인 타입은 필수입니다.")
+	String socialType,
+
+	@Schema(description = "소셜 ID", example = "563931436")
+	@NotBlank(message = "소셜 ID는 필수입니다.")
+	String socialId,
+
+	@Schema(description = "소셜 이메일", example = "zxc098@kakao.com")
+	@NotBlank(message = "소셜 이메일은 필수입니다.")
+	String socialEmail,
+
+	@Schema(description = "회원 이름", example = "홍길동")
+	@NotBlank(message = "회원 이름은 필수입니다.")
+	String name,
+
+	@Schema(description = "프로필 사진 URL", example = "asdf.png")
+	String profileImageUrl,
+
+	@Schema(description = "생년월일", example = "2025-01-01")
+	LocalDate birthday,
+
+	@Schema(description = "키", example = "300")
+	Double height,
+
+	@Schema(description = "몸무게", example = "255.5")
+	Double weight,
+
+	@Schema(description = "PT 목적", example = "체중 감량")
+	String goalContent,
+
+	@Schema(description = "주의사항", example = "가냘퍼요")
+	String cautionNote
+) {
+
+}
