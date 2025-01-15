bddaf6784cada09a0177cb260298b6afc2abb43b
fakerdeft <whakswp123@naver.com>
2025-01-15 23:44:52 +0900

    [TNT-83] refactor: 스키마 내용 수정

--- src/main/java/com/tnt/dto/member/request/OAuthLoginRequest.java
+++ src/main/java/com/tnt/dto/member/request/OAuthLoginRequest.java
@@ -3,19 +3,19 @@ package com.tnt.dto.member.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-@Schema(description = "소셜 로그인 요청")
+@Schema(description = "소셜 로그인 API 요청")
 public record OAuthLoginRequest(
-	@Schema(description = "소셜 로그인 타입", example = "kakao", type = "string", allowableValues = {"kakao", "apple"})
+	@Schema(description = "소셜 로그인 타입 (KAKAO / APPLE)", example = "KAKAO", allowableValues = {"KAKAO", "APPLE"})
 	@NotBlank(message = "소셜 로그인 타입은 필수입니다.")
 	String socialType,
 
-	@Schema(description = "소셜 액세스 토큰 (카카오 로그인 시)", example = "atweroiuhoresihsgfkn", type = "string")
+	@Schema(description = "소셜 액세스 토큰 (카카오 로그인 시)", example = "atweroiuhoresihsgfkn")
 	String socialAccessToken,
 
-	@Schema(description = "인가 코드 (애플 Android 로그인 시)", example = "1231231231231", type = "string")
+	@Schema(description = "인가 코드 (애플 Android 로그인 시)", example = "1231231231231")
 	String authorizationCode,
 
-	@Schema(description = "ID 토큰 (애플 iOS 로그인 시)", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...", type = "string")
+	@Schema(description = "ID 토큰 (애플 iOS 로그인 시)", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
 	String idToken
 ) {
 
