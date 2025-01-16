0b3a869c7f1e842d1e33403726e458711363fde2
fakerdeft <whakswp123@naver.com>
2025-01-17 00:31:16 +0900

    [TNT-84] feat: @Repository 제거

--- src/main/java/com/tnt/infrastructure/mysql/repository/member/MemberRepository.java
+++ src/main/java/com/tnt/infrastructure/mysql/repository/member/MemberRepository.java
@@ -3,12 +3,10 @@ package com.tnt.infrastructure.mysql.repository.member;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.tnt.domain.member.Member;
 import com.tnt.domain.member.SocialType;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findBySocialIdAndSocialTypeAndDeletedAtIsNull(String socialId, SocialType socialType);
