e5288292e30218e57a1e4217b0a177c37a1b59e2
fakerdeft <whakswp123@naver.com>
2025-01-17 00:31:05 +0900

    [TNT-84] feat: 세션 ID 추가

--- src/main/java/com/tnt/dto/member/response/SignUpResponse.java
+++ src/main/java/com/tnt/dto/member/response/SignUpResponse.java
@@ -4,14 +4,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "회원가입 API 응답")
 public record SignUpResponse(
+	@Schema(description = "회원 타입 (trainer / trainee)", example = "trainer")
+	String memberType,
+
+	@Schema(description = "세션 ID (1645365389)", example = "1645365389")
+	String sessionId,
+
 	@Schema(description = "회원 이름", example = "홍길동")
 	String name,
 
 	@Schema(description = "프로필 사진 URL", example = "asdf.png")
-	String profileImageUrl,
-
-	@Schema(description = "회원 타입 (trainer / trainee)", example = "trainer")
-	String memberType
+	String profileImageUrl
 ) {
 
 }
