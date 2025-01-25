c8c3c2612326b949f15333a87c09dd17390fee46
ymkim97 <rladudaud97@naver.com>
2025-01-25 13:16:26 +0900

    [TNT-106] feat: 트레이너 초대코드 재발급 기능 구현

--- src/main/java/com/tnt/application/trainer/TrainerService.java
+++ src/main/java/com/tnt/application/trainer/TrainerService.java
@@ -1,14 +1,16 @@
 package com.tnt.application.trainer;
 
-import static com.tnt.global.error.model.ErrorMessage.*;
+import static com.tnt.global.error.model.ErrorMessage.TRAINER_NOT_FOUND;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tnt.domain.trainer.Trainer;
 import com.tnt.dto.trainer.response.InvitationCodeResponse;
+import com.tnt.dto.trainer.response.InvitationCodeVerifyResponse;
 import com.tnt.global.error.exception.NotFoundException;
 import com.tnt.infrastructure.mysql.repository.trainer.TrainerRepository;
+import com.tnt.infrastructure.mysql.repository.trainer.TrainerSearchRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,23 +20,35 @@ import lombok.RequiredArgsConstructor;
 public class TrainerService {
 
 	private final TrainerRepository trainerRepository;
+	private final TrainerSearchRepository trainerSearchRepository;
 
 	public InvitationCodeResponse getInvitationCode(String memberId) {
-		Trainer trainer = getTrainer(memberId);
+		Trainer trainer = getTrainerWithMemberId(memberId);
 
-		return new InvitationCodeResponse(String.valueOf(trainer.getId()), trainer.getInvitationCode());
+		return new InvitationCodeResponse(trainer.getInvitationCode());
+	}
+
+	public InvitationCodeVerifyResponse verifyInvitationCode(String invitationCode) {
+		boolean isVerified = trainerRepository.findByInvitationCodeAndDeletedAtIsNull(invitationCode).isPresent();
+
+		return new InvitationCodeVerifyResponse(isVerified);
 	}
 
 	@Transactional
 	public InvitationCodeResponse reissueInvitationCode(String memberId) {
-		Trainer trainer = getTrainer(memberId);
+		Trainer trainer = getTrainerWithMemberId(memberId);
 		trainer.setNewInvitationCode();
 
-		return new InvitationCodeResponse(String.valueOf(trainer.getId()), trainer.getInvitationCode());
+		return new InvitationCodeResponse(trainer.getInvitationCode());
 	}
 
-	public Trainer getTrainer(String memberId) {
+	public Trainer getTrainerWithMemberId(String memberId) {
 		return trainerRepository.findByMemberIdAndDeletedAtIsNull(Long.valueOf(memberId))
+			.orElseThrow(() -> new NotFoundException(TRAINER_NOT_FOUND));
+	}
+
+	public Trainer getTrainerWithInvitationCode(String invitationCode) {
+		return trainerSearchRepository.findByInvitationCodeAndDeletedAtIsNull(invitationCode)
 			.orElseThrow(() -> new NotFoundException(TRAINER_NOT_FOUND));
 	}
 }
--- src/main/java/com/tnt/presentation/trainer/TrainerController.java
+++ src/main/java/com/tnt/presentation/trainer/TrainerController.java
@@ -1,8 +1,10 @@
 package com.tnt.presentation.trainer;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tnt.application.trainer.TrainerService;
 import com.tnt.dto.trainer.response.InvitationCodeResponse;
+import com.tnt.dto.trainer.response.InvitationCodeVerifyResponse;
 import com.tnt.global.auth.annotation.AuthMember;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -31,9 +34,16 @@ public class TrainerController {
 		return trainerService.getInvitationCode(memberId);
 	}
 
+	@Operation(summary = "트레이너 초대 코드 인증 API")
+	@ResponseStatus(OK)
+	@GetMapping("/invitation-code/verify/{code}")
+	public InvitationCodeVerifyResponse verifyInvitationCode(@PathVariable String code) {
+		return trainerService.verifyInvitationCode(code);
+	}
+
 	@Operation(summary = "트레이너 초대 코드 재발급 API")
 	@ResponseStatus(CREATED)
-	@PutMapping("/invitation-code")
+	@PutMapping("/invitation-code/reissue")
 	public InvitationCodeResponse reissueInvitationCode(@AuthMember String memberId) {
 		return trainerService.reissueInvitationCode(memberId);
 	}
