ce7f3674617e799f513f4080baca215928c4f095
ymkim97 <rladudaud97@naver.com>
2025-01-25 13:34:53 +0900

    [TNT-106] fix: profile에 따라 FCM 작동 다르도록 수정
    - local에서는 FCM 요청 없음
    - 그외에서는 FCM 요청

--- src/main/java/com/tnt/global/config/FcmConfig.java
+++ src/main/java/com/tnt/global/config/FcmConfig.java
@@ -1,12 +1,14 @@
 package com.tnt.global.config;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
 import com.google.auth.oauth2.GoogleCredentials;
@@ -14,20 +16,29 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 @Configuration
-@Profile({"prod", "dev"})
 public class FcmConfig {
 
 	@Value("${firebase.config.path}")
 	private String firebaseConfigPath;
 
 	@Bean
-	public FirebaseApp firebaseApp() throws IOException {
-		Resource serviceAccount = new ClassPathResource(firebaseConfigPath);
+	public FirebaseApp firebaseApp(Environment environment) throws IOException {
+		Resource serviceAccount;
+
+		if (Arrays.asList(environment.getActiveProfiles()).contains("local")) {
+			serviceAccount = new FileSystemResource(firebaseConfigPath);
+		} else {
+			serviceAccount = new ClassPathResource(firebaseConfigPath);
+		}
 
 		FirebaseOptions options = FirebaseOptions.builder()
 			.setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
 			.build();
 
-		return FirebaseApp.initializeApp(options);
+		if (FirebaseApp.getApps().isEmpty()) {
+			return FirebaseApp.initializeApp(options);
+		}
+
+		return FirebaseApp.getInstance();
 	}
 }
--- src/main/java/com/tnt/infrastructure/fcm/FcmAdapter.java
+++ src/main/java/com/tnt/infrastructure/fcm/FcmAdapter.java
@@ -1,31 +1,45 @@
 package com.tnt.infrastructure.fcm;
 
-import static com.tnt.global.error.model.ErrorMessage.*;
+import static com.tnt.global.error.model.ErrorMessage.FCM_FAILED;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
-import com.tnt.global.error.exception.TnTException;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class FcmAdapter {
 
-	public void sendNotificationByToken(String token, String title, String body, String clickActionUrl) {
+	private final Environment environment;
+
+	public void sendNotificationByToken(String token, String title, String body) {
+		String[] activeProfiles = environment.getActiveProfiles();
+		boolean isLocal = activeProfiles.length > 0 && "local".equals(activeProfiles[0]);
+
+		if (isLocal) {
+			log.info("현재 local 환경이므로 FCM 메시지를 전송하지 않습니다. [title: {}, body: {}]", title, body);
+			return;
+		}
+
 		Message message = Message.builder()
 			.setToken(token)
 			.setNotification(com.google.firebase.messaging.Notification.builder()
 				.setTitle(title)
 				.setBody(body)
 				.build())
-			.putData("click_action", clickActionUrl)
 			.build();
 
 		try {
 			FirebaseMessaging.getInstance().send(message);
 		} catch (FirebaseMessagingException e) {
-			throw new TnTException(FCM_FAILED, e);
+			log.error(FCM_FAILED.getMessage(), e); // FCM 전송 실패해도 외부 로직은 그대로 수행
 		}
 	}
 }
