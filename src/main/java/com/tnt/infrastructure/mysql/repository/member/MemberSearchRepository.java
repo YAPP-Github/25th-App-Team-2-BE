6a1a0fc9faa0dfc000bcb9554bb2abc35f2a6755
Manje Cho <whakswp123@naver.com>
2025-02-03 22:29:43 +0900

    [TNT-204] feat: 회원 조회 기능 구현 (#40)
    * [TNT-204] feat: 회원 조회 기능 구현
    
    * [TNT-204] refactor: 스웨거 내용 수정
    
    * [TNT-204] refactor: nullable false로 수정
    
    * [TNT-204] refactor: 불필요한 연산 제거
    
    * [TNT-204] refactor: 리턴 타입 Map으로 수정

--- src/main/java/com/tnt/infrastructure/mysql/repository/member/MemberSearchRepository.java
+++ src/main/java/com/tnt/infrastructure/mysql/repository/member/MemberSearchRepository.java
@@ -1,13 +1,19 @@
 package com.tnt.infrastructure.mysql.repository.member;
 
 import static com.tnt.domain.member.QMember.member;
+import static com.tnt.domain.trainee.QPtGoal.ptGoal;
+import static com.tnt.domain.trainee.QTrainee.trainee;
+import static com.tnt.domain.trainer.QTrainer.trainer;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.group.GroupBy;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tnt.dto.member.MemberProjection;
+import com.tnt.dto.member.QMemberProjection_MemberInfoDto;
 import com.tnt.dto.member.QMemberProjection_MemberTypeDto;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +24,27 @@ public class MemberSearchRepository {
 
 	private final JPAQueryFactory jpaQueryFactory;
 
-	public Optional<MemberProjection.MemberTypeDto> findMemberTypeByMemberId(Long memberId) {
+	public Optional<MemberProjection.MemberInfoDto> findMemberInfo(Long memberId) {
+		Map<Long, MemberProjection.MemberInfoDto> result = jpaQueryFactory
+			.from(member)
+			.leftJoin(trainer).on(trainer.member.id.eq(member.id), trainer.deletedAt.isNull())
+			.leftJoin(trainee).on(trainee.member.id.eq(member.id), trainee.deletedAt.isNull())
+			.leftJoin(ptGoal).on(ptGoal.traineeId.eq(trainee.id), ptGoal.deletedAt.isNull())
+			.where(
+				member.id.eq(memberId),
+				member.deletedAt.isNull()
+			)
+			.transform(GroupBy.groupBy(member.id).as(
+				new QMemberProjection_MemberInfoDto(member.name, member.email, member.profileImageUrl, member.birthday,
+					member.memberType, member.socialType, trainer.invitationCode, trainee.height, trainee.weight,
+					trainee.cautionNote, GroupBy.list(ptGoal.content)
+				)
+			));
+
+		return Optional.ofNullable(result.get(memberId));
+	}
+
+	public Optional<MemberProjection.MemberTypeDto> findMemberType(Long memberId) {
 		return Optional.ofNullable(jpaQueryFactory
 			.select(new QMemberProjection_MemberTypeDto(member.memberType))
 			.from(member)
