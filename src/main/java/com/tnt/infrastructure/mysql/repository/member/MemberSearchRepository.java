fb2950be5d6f0ddcf60b38e0f6f824a404b509fb
fakerdeft <whakswp123@naver.com>
2025-02-03 22:19:22 +0900

    [TNT-204] refactor: 리턴 타입 Map으로 수정

--- src/main/java/com/tnt/infrastructure/mysql/repository/member/MemberSearchRepository.java
+++ src/main/java/com/tnt/infrastructure/mysql/repository/member/MemberSearchRepository.java
@@ -1,12 +1,11 @@
 package com.tnt.infrastructure.mysql.repository.member;
 
-import static com.querydsl.core.types.Projections.constructor;
 import static com.tnt.domain.member.QMember.member;
 import static com.tnt.domain.trainee.QPtGoal.ptGoal;
 import static com.tnt.domain.trainee.QTrainee.trainee;
 import static com.tnt.domain.trainer.QTrainer.trainer;
 
-import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
@@ -14,6 +13,7 @@ import org.springframework.stereotype.Repository;
 import com.querydsl.core.group.GroupBy;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tnt.dto.member.MemberProjection;
+import com.tnt.dto.member.QMemberProjection_MemberInfoDto;
 import com.tnt.dto.member.QMemberProjection_MemberTypeDto;
 
 import lombok.RequiredArgsConstructor;
@@ -25,7 +25,7 @@ public class MemberSearchRepository {
 	private final JPAQueryFactory jpaQueryFactory;
 
 	public Optional<MemberProjection.MemberInfoDto> findMemberInfo(Long memberId) {
-		List<MemberProjection.MemberInfoDto> result = jpaQueryFactory
+		Map<Long, MemberProjection.MemberInfoDto> result = jpaQueryFactory
 			.from(member)
 			.leftJoin(trainer).on(trainer.member.id.eq(member.id), trainer.deletedAt.isNull())
 			.leftJoin(trainee).on(trainee.member.id.eq(member.id), trainee.deletedAt.isNull())
@@ -34,14 +34,14 @@ public class MemberSearchRepository {
 				member.id.eq(memberId),
 				member.deletedAt.isNull()
 			)
-			.transform(GroupBy.groupBy(member.id).list(
-				constructor(MemberProjection.MemberInfoDto.class, member.name, member.email, member.profileImageUrl,
-					member.birthday, member.memberType, member.socialType, trainer.invitationCode, trainee.height,
-					trainee.weight, trainee.cautionNote, GroupBy.list(ptGoal.content)
+			.transform(GroupBy.groupBy(member.id).as(
+				new QMemberProjection_MemberInfoDto(member.name, member.email, member.profileImageUrl, member.birthday,
+					member.memberType, member.socialType, trainer.invitationCode, trainee.height, trainee.weight,
+					trainee.cautionNote, GroupBy.list(ptGoal.content)
 				)
 			));
 
-		return result.isEmpty() ? Optional.empty() : Optional.of(result.get(0));
+		return Optional.ofNullable(result.get(memberId));
 	}
 
 	public Optional<MemberProjection.MemberTypeDto> findMemberType(Long memberId) {
