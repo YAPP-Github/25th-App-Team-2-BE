91b0d701ab624df575cad42b56bbb55428e8209d
fakerdeft <whakswp123@naver.com>
2025-02-11 12:33:23 +0900

    [TNT-217] feat: 식단 등록 기능 구현

--- src/main/java/com/tnt/dto/trainee/request/CreateDietRequest.java
+++ src/main/java/com/tnt/dto/trainee/request/CreateDietRequest.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
+@Schema(description = "식단 등록 API 요청")
 public record CreateDietRequest(
 	@Schema(description = "식사 날짜", example = "2025-01-01", nullable = true)
 	@PastOrPresent(message = "식사 날짜는 현재거나 과거 날짜여야 합니다.")
